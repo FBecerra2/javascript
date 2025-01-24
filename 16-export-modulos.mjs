9e3effcd03c9349fb50d80ad26bd1a69c02a7d74
Facundo Becerra <facundo.g.becerra@gmail.com>
2025-01-24 08:12:19 +0100

    modulos

---
+++ 16-export-modulos.mjs
@@ -0,0 +1,13 @@
+//exportacion de modulo
+export function add(a, b) {
+  return a + b;
+}
+
+//Propiedades de exportacion
+
+export const PI = Math.PI;
+
+//Exportacion por defecto
+export default function substarct(a, b) {
+  return a - b;
+}
---
+++ 17-import-modules.mjs
@@ -0,0 +1,8 @@
+import { add, PI } from './16-export-modulos.mjs';
+import substract from './16-export-modulos.mjs';
+
+console.log(add(1, 2));
+
+console.log(PI);
+
+console.log(substract(1, 2));
